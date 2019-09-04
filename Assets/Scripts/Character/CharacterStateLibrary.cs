c082d3af90f71401f108dc7de85b10781a085a2e
kerryoo <kerr.yen.yoo@vanderbilt.edu>
2019-09-04 10:01:14 -0500

    added singleton pattern to character state library for each state

--- Assets/Scripts/Character/CharacterStateLibrary.cs
+++ Assets/Scripts/Character/CharacterStateLibrary.cs
@@ -4,14 +4,49 @@ using UnityEngine;
 
 public class CharacterStateLibrary : MonoBehaviour
 {
-    IdleState idleState;
-    WalkingState walkingState;
+    public CharacterState currCharacterState;
+    
+    private IdleState m_IdleState;
+    public IdleState IdleState
+    {
+        get
+        {
+            if (m_IdleState == null)
+            {
+                m_IdleState = new IdleState();
+            }
+            return m_IdleState;
+        }
+    }
+
+    private WalkingState m_WalkingState;
+    public WalkingState WalkingState
+    {
+        get
+        {
+            if (m_WalkingState == null)
+            {
+                m_WalkingState = new WalkingState();
+            }
+            return m_WalkingState;
+        }
+    }
+
+    private SpecialMovementState m_SpecialMovementState;
+    public SpecialMovementState SpecialMovementState
+    {
+        get
+        {
+            if (m_SpecialMovementState = null)
+            {
+                m_SpecialMovementState = new SpecialMovementState();
+            }
+            return m_SpecialMovementState;
+        }
+    }
 
     private void Awake()
     {
-        idleState = new IdleState();
-        walkingState = new WalkingState();
-
 
     }
 
